83f31949a4426abb294fc57259bf1c3e12f86f81
DoubleHasan <hsnov.2005@gmail.com>
2026-04-14 20:36:52 +0400

    Fix Render port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
@@ -127,5 +127,5 @@ app.post("/contact", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
